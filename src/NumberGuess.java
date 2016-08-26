a0dec91b3cf6f70a2716641feb9744f393ee287e
GoodJuan <190141@hkis.edu.hk>
2016-08-26 14:59:39 +0800

    Final update

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 
@@ -7,18 +8,22 @@ public class NumberGuess {
 	static Random rand = new Random();
 	static int start;
 	static int numberGuess;
-	static int[] randomArray = new int[4];
-	static int[] userArray = new int[4];
-	static int[] testArray = new int[]{1,2,3,4};
+	static int[] randomArray = new int[5];
+	static int[] userArray = new int[5];
+	static int[] testArray = new int[]{1,1,3,4};
+	static int[] repeatChecker = new int[10];
+	static int repeatCheckerVar;
 	static int triesLeft = 8;
 	static int howManyTimes;
+	static int howManyTimes2;
+	
 
 	public static int[] createNumArray(){
 		int randomNum; 
 		for (int i = 0; i <= 3; i++){
 			randomNum = rand.nextInt(9)+1;
 			randomArray[i] = randomNum;
-			System.out.println(randomNum);
+			//System.out.println(randomNum);
 		}
 		return randomArray;
 		
@@ -35,27 +40,98 @@ public class NumberGuess {
 		
 	}
 	
+	public static boolean contains(int[] arr, int i) {
+	      for (int n : arr) {
+	         if (i == n) {
+	            return true;
+	         }
+	      }
+	      return false;
+	   }
+	
+	public static void goAgainMethod(){
+		int goAgain;
+		System.out.println("Please input 1. to go again, and 2. to exit.\n\n\n");
+		goAgain = input.nextInt();
+		switch (goAgain){
+		
+		case 1: main(null);
+		
+		case 2: System.exit(0);
+		
+		default: System.out.println("Invalid input");
+				goAgainMethod();
+		}
+	}
+	
+	
 	public static void compareArrays(){
+		howManyTimes = 0;
+		howManyTimes2 = 0;
+		if (Arrays.equals(userArray, randomArray)){
+			System.out.println("Congrats! You win!");
+			goAgainMethod();
+			}
+			
 		triesLeft--;
 		for (int z = 0; z <= 3; z++){
 			int getNumUser = userArray[z];
 			int getNumComp = randomArray[z];
-			int getNumTest = testArray[z];
-			if (getNumUser == getNumTest){
+			//int getNumTest = testArray[z];
+			if (getNumUser == getNumComp){
+				//System.out.println(getNumUser);
+				//System.out.println(getNumComp);
 				howManyTimes++;
-				System.out.println("You have placed the correct number " + howManyTimes + " times");
 			}
 		}
+		if (howManyTimes >= 1){
+			System.out.println("You have placed the correct number in the correct place " + howManyTimes + " time(s).");
+		}
+		for (int a = 0; a <= 3; a++){
+			for (int b = 0; b <= 3; b++){
+				int getNumUser2 = userArray[a];
+				int getNumComp2 = randomArray[b];
+				//int getNumTest2 = testArray[b];
+				if (getNumUser2 == getNumComp2){
+					if (contains(repeatChecker, getNumUser2)){
+						continue;
+					}
+					System.out.println(getNumUser2);
+					System.out.println(getNumComp2);
+					repeatChecker[repeatCheckerVar] = userArray[a];
+					howManyTimes2++;
+					
+				}
+				
+			}
+		}
+		if (howManyTimes2 >= 1){
+			System.out.println("In your set of numbers, " + howManyTimes2 + " of your numbers are correct.");
+		}
+		if (triesLeft == 0){
+			System.out.println("You failed!");
+			System.out.println("The array of numbers you were guess for is: " + Arrays.toString(randomArray));
+			goAgainMethod();
+			
+		}
+		System.out.println("You have " + triesLeft + " tries left.\n\n");
+		gameStart();
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Welcome to mastermind!");
 		System.out.println("Enter 1. to start or 2. to exit.");
 		start = input.nextInt();
 		
 		//System.out.println(randomNum);
 		if (start  == 1){
+			createNumArray();
+			randomArray = createNumArray();
+			for (int k = 0; k < 4; k++){
+				System.out.println(randomArray[k]);
+			}
 			gameStart();
 		}
 		else if (start == 2){
